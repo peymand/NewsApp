90f8bf2d5f11245246032eebc0ad03f8d6099e73
Peyman <peymandodangeh@gmail.com>
2018-03-15 10:27:44 +0330

    complete the show user profile feature.

--- src/com/kahkeshan/controllers/UserProfileController.java
+++ src/com/kahkeshan/controllers/UserProfileController.java
@@ -1,5 +1,8 @@
 package com.kahkeshan.controllers;
 
+import com.kahkeshan.models.User;
+import com.nicico.services.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -9,9 +12,20 @@ import java.io.IOException;
 
 @WebServlet("/showUserProfile.do")
 public class UserProfileController extends HttpServlet {
+    UserService userService;
+
+    public UserProfileController() {
+        userService = new UserService();
+    }
+
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        String id = req.getParameter("id");
+
+        String idParam = req.getParameter("id");
+        int id = Integer.parseInt(idParam);
+       User user = userService.getUser(id);
+       req.setAttribute("user",user);
+       req.getRequestDispatcher("/user/profile.jsp").forward(req,res);
 
     }
 }
--- src/com/nicico/services/UserService.java
+++ src/com/nicico/services/UserService.java
@@ -1,4 +1,17 @@
 package com.nicico.services;
 
+import com.kahkeshan.models.User;
+import com.nicico.data.UserDAO;
+import com.nicico.data.UserDAOImpl;
+
 public class UserService {
+    UserDAO  userDAO;
+
+    public UserService() {
+        userDAO = new UserDAOImpl();
+    }
+
+    public User getUser(int id){
+        return userDAO.getUser(id);
+    }
 }
